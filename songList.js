c52fca56b0506d8c7b1831956c1cbd6853082a98
Musical Coder <github@musicalcoder.com>
2018-06-04 16:47:20 -0400

    song split changes

---
+++ songList.js
@@ -0,0 +1,338 @@
+varSongList = [
+{
+"file":"allsummer",
+"title":"All Summer Long",
+"project":"ttg"
+},
+{
+"file":"amergirl",
+"title":"American Girl",
+"project":"all"
+},
+{
+"file":"anotherbite",
+"title":"Another One Bites The Dust",
+"project":"ttg"
+},
+{
+"file":"badgirlfriend",
+"title":"Bad Girlfriend",
+"project":"ttg"
+},
+{
+"file":"billiejean",
+"title":"Billie Jean",
+"project":"ttg"
+},
+{
+"file":"bluecolor",
+"title":"Blue Ain't Your Color",
+"project":"ttg"
+},
+{
+"file":"breakchain",
+"title":"Breaking The Chains",
+"project":"ttg"
+},
+{
+"file":"brickhouse",
+"title":"Brick House",
+"project":"ttg"
+},
+{
+"file":"browneye",
+"title":"Brown Eyed Girl",
+"project":"ttg"
+},
+{
+"file":"cakeocean",
+"title":"Cake By The Ocean",
+"project":"ttg"
+},
+{
+"file":"centerfold",
+"title":"Centerfold",
+"project":"ttg"
+},
+{
+"file":"dontstop",
+"title":"Don't Stop Believin'",
+"project":"ttg"
+},
+{
+"file":"doublevision",
+"title":"Double Vision",
+"project":"ttg"
+},
+{
+"file":"entersand",
+"title":"Enter Sandman",
+"project":"ttg"
+},
+{
+"file":"everybody",
+"title":"Everybody Wants You",
+"project":"ttg"
+},
+{
+"file":"faith",
+"title":"Faith",
+"project":"all"
+},
+{
+"file":"fatbottom",
+"title":"Fat Bottom Girls",
+"project":"ttg"
+},
+{
+"file":"feelmakelove",
+"title":"Feel Like Makin' Love",
+"project":"ttg"
+},
+{
+"file":"fightright",
+"title":"Fight For Your Right",
+"project":"ttg"
+},
+{
+"file":"footloose",
+"title":"Footloose",
+"project":"ttg"
+},
+{
+"file":"gimme3steps",
+"title":"Gimme 3 Steps",
+"project":"ttg"
+},
+{
+"file":"getoverit",
+"title":"Get Over It",
+"project":"ttg"
+},
+{
+"file":"sloopy",
+"title":"Hang On Sloopy",
+"project":"ttg"
+},
+{
+"file":"harder2breathe",
+"title":"Harder To Breathe",
+"project":"ttg"
+},
+{
+"file":"highground",
+"title":"Higher Ground",
+"project":"ttg"
+},
+{
+"file":"honkytonkwoman",
+"title":"Honkytonk Woman",
+"project":"all"
+},
+{
+"file":"holdmyhand",
+"title":"Hold My Hand",
+"project":"mbp"
+},
+{
+"file":"hungrywolf",
+"title":"Hungry Like The Wolf",
+"project":"ttg"
+},
+{
+"file":"hurtgood",
+"title":"Hurt So Good",
+"project":"ttg"
+},
+{
+"file":"fallinlove",
+"title":"I'll Fall In Love Again",
+"project":"ttg"
+},
+{
+"file":"inmydreams",
+"title":"In My Dreams",
+"project":"ttg"
+},
+{
+"file":"nodoctor",
+"title":"I Don't Need No Doctor",
+"project":"ttg"
+},
+{
+"file":"jessiegirl",
+"title":"Jessie's Girl",
+"project":"all"
+},
+{
+"file":"keephands",
+"title":"Keep Your Hands To Yourself",
+"project":"ttg"
+},
+{
+"file":"learn2fly",
+"title":"Learn To Fly",
+"project":"ttg"
+},
+{
+"file":"livmidnight",
+"title":"Living After Midnight",
+"project":"ttg"
+},
+{
+"file":"livprayer",
+"title":"Living On A Prayer",
+"project":"ttg"
+},
+{
+"file":"longtrain",
+"title":"Long Train Running",
+"project":"ttg"
+},
+{
+"file":"loveshack",
+"title":"Love Shack",
+"project":"ttg"
+},
+{
+"file":"margaritaville",
+"title":"Margaritaville",
+"project":"ttg"
+},
+{
+"file":"moredrink",
+"title":"More I Drink",
+"project":"ttg"
+},
+{
+"file":"mustangsally",
+"title":"Mustang Sally",
+"project":"ttg"
+},
+{
+"file":"ownworst",
+"title":"My Own Worst Enemy",
+"project":"ttg"
+},
+{
+"file":"oneilove",
+"title":"The One I Love",
+"project":"ttg"
+},
+{
+"file":"funkymusic",
+"title":"Play That Funky Music",
+"project":"ttg"
+},
+{
+"file":"rockusa",
+"title":"R.O.C.K. In The USA",
+"project":"ttg"
+},
+{
+"file":"rockymtnway",
+"title":"Rocky Mountain Way",
+"project":"ttg"
+},
+{
+"file":"sevenbridges",
+"title":"Seven Bridges Road",
+"project":"ttg"
+},
+{
+"file":"shoutoutloud",
+"title":"Shout It Out Loud",
+"project":"ttg"
+},
+{
+"file":"shutupdance",
+"title":"Shut Up And Dance",
+"project":"ttg"
+},
+{
+"file":"wonderful",
+"title":"Some Kind Of Wonderful",
+"project":"ttg"
+},
+{
+"file":"savehorse",
+"title":"Save A Horse",
+"project":"ttg"
+},
+{
+"file":"sepways",
+"title":"Separate Ways",
+"project":"ttg"
+},
+{
+"file":"stuckmiddle",
+"title":"Stuck In The Middle",
+"project":"all"
+},
+{
+"file":"takecarebiz",
+"title":"Taking Care Of Business",
+"project":"ttg"
+},
+{
+"file":"tenwhiskey",
+"title":"Tennessee Whiskey",
+"project":"ttg"
+},
+{
+"file":"tiemother",
+"title":"Tie Your Mother Down",
+"project":"ttg"
+},
+{
+"file":"turnloose",
+"title":"Turn Me Loose",
+"project":"ttg"
+},
+{
+"file":"turnjap",
+"title":"Turning Japanese",
+"project":"ttg"
+},
+{
+"file":"underbridge",
+"title":"Under The Bridge",
+"project":"ttg"
+},
+{
+"file":"upfunk",
+"title":"Uptown Funk",
+"project":"ttg"
+},
+{
+"file":"warpig",
+"title":"War Pigs",
+"project":"ttg"
+},
+{
+"file":"likeaboutyou",
+"title":"What I Like About You",
+"project":"ttg"
+},
+{
+"file":"wildwest",
+"title":"Wild Wild West",
+"project":"ttg"
+},
+{
+"file":"workweekend",
+"title":"Working For The Weekend",
+"project":"ttg"
+},
+{
+"file":"tookthewords",
+"title":"You Took The Words",
+"project":"ttg"
+},
+{
+"file":"lethercry",
+"title":"Let Her Cry",
+"project":"mbp"
+}
+]
+
