9f260fc52dc66ad6c6496385d807001f31f3cb52
Musical Coder <github@musicalcoder.com>
2018-06-04 15:22:22 -0400

    updated footer button to use javascript

--- variables.js
+++ variables.js
@@ -1,11 +1,16 @@
-$html = '<div class="col-2">' +
+$headHtml = '<div class="col-2">' +
     '<br/><a class="btn btn-primary size" href="./index.html">HOME</a>' +
     '</div>' +
     '<div class="col-10">' +
     '<br/><div class="songTitle" id="songTitle"></div>' +
     '</div>';
 
+$footHtml = '<div class="col-2">' +
+	'<a class="btn btn-primary size" href="./index.html">HOME</a>' +
+	'</div>';
+	
 function LoadTitle(title) {
-    $('#headerRow').html($html);
+    $('#headerRow').html($headHtml);
     $('#songTitle').html(title);
+	$('#footerRow').html($footHtml)
 }
